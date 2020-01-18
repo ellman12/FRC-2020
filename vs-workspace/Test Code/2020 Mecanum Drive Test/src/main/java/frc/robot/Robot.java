f0257f9818d4011d828238d9ef94266391227c16
ellman12 <ellduc4@gmail.com>
2020-01-18 11:20:08 -0600

    Update Robot.java

--- vs-workspace/Test Code/2020 Mecanum Drive Test/src/main/java/frc/robot/Robot.java
+++ vs-workspace/Test Code/2020 Mecanum Drive Test/src/main/java/frc/robot/Robot.java
@@ -12,15 +12,39 @@
 //
 /////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////
-
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
 
 public class Robot extends TimedRobot {
 
+  // Creating the drive motors.
+  WPI_TalonFX frontLeftMotor, frontRightMotor, backLeftMotor, backRightMotor;
+
+  // Creatingn the Mecanum Drive.
+  MecanumDrive mecanumDrive;
+
+  // Creating the PS4 controller.
+  Joystick PS4;
+
   @Override
   public void robotInit() {
+
+    // Dummy IDs for right now!
+    frontLeftMotor = new WPI_TalonFX(1);
+    frontRightMotor = new WPI_TalonFX(2);
+    backLeftMotor = new WPI_TalonFX(3);
+    backRightMotor = new WPI_TalonFX(4);
+
+    // Adding the drive motors to the Mecanum Drive.
+    mecanumDrive = new MecanumDrive(frontLeftMotor, backLeftMotor, frontRightMotor, backLeftMotor);
+
+    // Assigning the PS4 controller the ID of 0.
+    PS4 = new Joystick(0);
   }
 
   @Override
@@ -29,7 +53,6 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousInit() {
-
   }
 
   @Override
@@ -39,6 +62,9 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
+
+    // Drive the robot's Mecanum Drive with the PS4 controller.
+    mecanumDrive.driveCartesian(PS4.getY(), PS4.getX(), PS4.getZ());
   }
 
   @Override
