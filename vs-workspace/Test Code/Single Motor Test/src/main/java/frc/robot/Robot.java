51cc050fa3293e2fcaaed7e0a47a031b1c0ccffc
ellman12 <ellduc4@gmail.com>
2020-02-01 17:23:35 -0600

    Added 2 Spark Maxes to Single Motor Test (SMT).

--- vs-workspace/Test Code/Single Motor Test/src/main/java/frc/robot/Robot.java
+++ vs-workspace/Test Code/Single Motor Test/src/main/java/frc/robot/Robot.java
@@ -1,3 +1,18 @@
+/////////////////////////////////////////////////////////////////////
+// File: Robot.java
+/////////////////////////////////////////////////////////////////////
+//
+// Purpose: Program used for testing motor speeds.
+//
+// Authors: Elliott DuCharme of FRC Team #5914.
+//
+// Environment: Microsoft VSCode Java
+//
+// Remarks: Made for 2020 competition; probably won't be useful for
+// future seasons.
+//
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
 package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
@@ -10,13 +25,17 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends TimedRobot {
 
-  final int TEST_FALCON_PORT = 1;
-  final int TEST_SPARK_MAX_PORT = 2;
+  final int TEST_SPARK_MAX1_PORT = 1;
+  final int TEST_SPARK_MAX2_PORT = 2;
+  final int TEST_FALCON_PORT = 3;
+
+  CANSparkMax testSparkMax1 = new CANSparkMax(TEST_SPARK_MAX1_PORT, MotorType.kBrushless);
+  CANSparkMax testSparkMax2 = new CANSparkMax(TEST_SPARK_MAX2_PORT, MotorType.kBrushless);
 
   WPI_TalonFX testFalcon = new WPI_TalonFX(TEST_FALCON_PORT);
 
-  CANSparkMax testSparkMax = new CANSparkMax(TEST_SPARK_MAX_PORT, MotorType.kBrushless);
-
+  double SparkMax1Speed;
+  double SparkMax2Speed;
   double Falcon500Speed;
 
   private static final String kDefaultAuto = "Default";
@@ -64,6 +83,12 @@ public class Robot extends TimedRobot {
 
     SmartDashboard.getNumber("Falcon 500 SPeed", Falcon500Speed);
     SmartDashboard.putNumber("Falcon 500 Speed", Falcon500Speed);
+
+    SmartDashboard.getNumber("Spark Max 1 SPeed", SparkMax1Speed);
+    SmartDashboard.putNumber("Spark Max 1 Speed", SparkMax1Speed);
+
+    SmartDashboard.getNumber("Spark Max 2 SPeed", SparkMax2Speed);
+    SmartDashboard.putNumber("Spark Max 2 Speed", SparkMax2Speed);
 
   }
 
