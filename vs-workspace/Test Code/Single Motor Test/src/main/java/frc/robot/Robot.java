63c3d201e26e379171c9a47a61639f381b323e1d
ellman12 <ellduc4@gmail.com>
2020-02-01 18:56:40 -0600

    Update Robot.java

--- vs-workspace/Test Code/Single Motor Test/src/main/java/frc/robot/Robot.java
+++ vs-workspace/Test Code/Single Motor Test/src/main/java/frc/robot/Robot.java
@@ -29,8 +29,10 @@ public class Robot extends TimedRobot {
 
   // Magic numbers for motor ports.
   final int TEST_SPARK_MAX1_PORT = 1;
-  final int TEST_SPARK_MAX2_PORT = 2;
-  final int TEST_FALCON_PORT = 3;
+  final int TEST_SPARK_MAX2_PORT = 3;
+  final int TEST_FALCON_PORT = 2;
+
+  // String nullVarTest = null;
 
   // Creating the 2 Spark Maxes.
   CANSparkMax testSparkMax1 = new CANSparkMax(TEST_SPARK_MAX1_PORT, MotorType.kBrushless);
@@ -40,63 +42,77 @@ public class Robot extends TimedRobot {
   WPI_TalonFX testFalcon = new WPI_TalonFX(TEST_FALCON_PORT);
 
   // Doubles used for controlling the motor speeds through SD.
-  double SparkMax1Speed;
-  double SparkMax2Speed;
-  double Falcon500Speed;
+  double SparkMax1Speed = 0;
+  double SparkMax2Speed = 0;
+  double Falcon500Speed = 0;
 
   // SD stuff.
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  public static final String sparkMax1SpeedChoice = "sparkMax1SpeedChoice";
+  public SendableChooser<String> sendChooserSpeedSparkMax1 = new SendableChooser<>();
+
+  public static final String sparkMax2SpeedChoice = "sparkMax2SpeedChoice";
+  public SendableChooser<String> sendChooserSpeedSparkMax2 = new SendableChooser<>();
+
+  public static final String falconSpeedChoice = "falconSpeedChoice";
+  public SendableChooser<String> sendChooserSpeedFalcon = new SendableChooser<>();
 
   @Override
   public void robotInit() {
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
+
   }
 
   @Override
   public void robotPeriodic() {
+
+    // Temporary test.
+    // try {
+
+    // System.out.println("nullVarTest: " + nullVarTest);
+
+    // } catch (NullPointerException e) {
+
+    // System.out.print("NullPointerException Caught");
+    // }
+
   }
 
   @Override
   public void autonomousInit() {
-
-    m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
 
   }
 
   @Override
   public void autonomousPeriodic() {
 
-    switch (m_autoSelected) {
-    case kCustomAuto:
-      // Put custom auto code here
-      break;
-    case kDefaultAuto:
-    default:
-      // Put default auto code here
-      break;
-    }
-
   }
 
   @Override
   public void teleopPeriodic() {
 
+    SparkMax1Speed = SmartDashboard.getNumber(sparkMax1SpeedChoice, 100) / 100;
+    SmartDashboard.putNumber(sparkMax1SpeedChoice, SparkMax1Speed * 100);
+
+    SparkMax2Speed = SmartDashboard.getNumber(sparkMax2SpeedChoice, 100) / 100;
+    SmartDashboard.putNumber(sparkMax2SpeedChoice, SparkMax2Speed * 100);
+
+    Falcon500Speed = SmartDashboard.getNumber(falconSpeedChoice, 100) / 100;
+    SmartDashboard.putNumber(falconSpeedChoice, Falcon500Speed * 100);
+
     // Getting and setting motor speeds.
-    SmartDashboard.getNumber("Falcon 500 SPeed", Falcon500Speed);
-    SmartDashboard.putNumber("Falcon 500 Speed", Falcon500Speed);
+    // SmartDashboard.putData("Falcon 500 SPeed", Falcon500Speed);
+    // SmartDashboard.putNumber("Falcon 500 Speed", Falcon500Speed);
 
-    SmartDashboard.getNumber("Spark Max 1 SPeed", SparkMax1Speed);
-    SmartDashboard.putNumber("Spark Max 1 Speed", SparkMax1Speed);
+    // SmartDashboard.getNumber("Spark Max 1 SPeed", SparkMax1Speed);
+    // SmartDashboard.putNumber("Spark Max 1 Speed", SparkMax1Speed);
 
-    SmartDashboard.getNumber("Spark Max 2 SPeed", SparkMax2Speed);
-    SmartDashboard.putNumber("Spark Max 2 Speed", SparkMax2Speed);
+    // SmartDashboard.getNumber("Spark Max 2 SPeed", SparkMax2Speed);
+    // SmartDashboard.putNumber("Spark Max 2 Speed", SparkMax2Speed);
+
+    testFalcon.set(Falcon500Speed);
+
+    testSparkMax1.set(SparkMax1Speed);
+
+    testSparkMax2.set(SparkMax2Speed);
 
   }
 
