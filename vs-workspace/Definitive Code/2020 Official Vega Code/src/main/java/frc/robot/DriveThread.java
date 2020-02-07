1a6ce0f7b9d55bfd716f8312ab9655149ef65ab8
ellman12 <ellduc4@gmail.com>
2020-02-06 20:23:45 -0600

    Started making the DriveThread; started learning about Threads, and threw a ton of comments in.

--- vs-workspace/Definitive Code/2020 Official Vega Code/src/main/java/frc/robot/DriveThread.java
+++ vs-workspace/Definitive Code/2020 Official Vega Code/src/main/java/frc/robot/DriveThread.java
@@ -1,36 +1,51 @@
 /////////////////////////////////////////////////////////////////////
-// File: 
+// File: DriveThread.java
 /////////////////////////////////////////////////////////////////////
 //
-// Purpose: 
+// Purpose: Defines the thread class responsible for robot movement
+// forward, backwards, left, and right during autonomous.
+// All autonomous movements and commands are created in
+// Autonomous.java, and are called in here.
+// Having them in a separate file declutters this already-long file.
 //
-// Authors: 
+// Authors: Elliott DuCharme, Larry Basegio, and Noah Stigeler.
 //
 // Environment: Microsoft VSCode Java
 //
-// Remarks: 
+// Remarks:
 //
 /////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////
 package frc.robot;
 
+// Creating the class that implements the Runnable interface.
 class DriveThread implements Runnable {
-    String name;
-    Thread t;
-    Runtime r = Runtime.getRuntime();
+
+    // Creating instance of the Thread class by
+    // creating a thread (reserving memory for this object).
+    Thread driveThread;
+
+    // Getting a reference to the Runtime class.
+    // We use this stuff for garbage collection.
+    // According to page 461 chapter 11 of Java: The Complete Reference 9th edition
+    // by Herbert Schildt, you can't instantiate a Runtime object.
+    // But, you can get a reference to it. Using this, you can control
+    // the state and behavior of the Java Virtual Machine.
+    // Lots of cool functions in this section: totalMemory(), freeMemory(), etc.
+    // Worth a look.
+    Runtime runtime = Runtime.getRuntime();
 
     // DriveThread constructor.
-    DriveThread(String threadname) {
+    // String threadName is what is inputted when the Thread is created in the code.
+    // Normally, you would have like "DriveThread" or something.
+    DriveThread(String threadName) {
 
-        name = threadname;
-		t = new Thread(this, name);
-		t.start(); // Start the thread
+        driveThread = new Thread(this, threadName); // Actually creating the Thread.
+        driveThread.start(); // Start the thread.
 
     }
 
     public void run() {
-
-        
 
     }
 
