60be2f2395959121931d43a4680e2183ff414176
ellman12 <ellduc4@gmail.com>
2020-02-07 19:48:36 -0600

    Update Robot.java

--- vs-workspace/Test Code/2020 DriveThread Test/src/main/java/frc/robot/Robot.java
+++ vs-workspace/Test Code/2020 DriveThread Test/src/main/java/frc/robot/Robot.java
@@ -111,11 +111,11 @@ public class Robot extends TimedRobot {
   // Joystick axes.
   double PS4LeftXAxis, PS4LeftYAxis, PS4RightXAxis, PS4RightYAxis, PS4LeftAnalogTrigger, PS4RightAnalogTrigger;
 
-  // Magic numebrs for PS4 controller axes.
+  // Magic numbers for PS4 controller axes.
   final int LEFT_X_AXIS_PORT = 0;
   final int LEFT_Y_AXIS_PORT = 1;
   final int RIGHT_X_AXIS_PORT = 2;
-  final int RIGHTT_Y_AXIS_PORT = 5;
+  final int RIGHT_Y_AXIS_PORT = 5;
 
   @Override
   public void robotInit() {
@@ -196,7 +196,7 @@ public class Robot extends TimedRobot {
     PS4LeftXAxis = PS4.getRawAxis(LEFT_X_AXIS_PORT);
     PS4LeftYAxis = PS4.getRawAxis(LEFT_Y_AXIS_PORT);
     PS4RightXAxis = PS4.getRawAxis(RIGHT_X_AXIS_PORT);
-    PS4RightYAxis = PS4.getRawAxis(RIGHTT_Y_AXIS_PORT);
+    PS4RightYAxis = PS4.getRawAxis(RIGHT_Y_AXIS_PORT);
 
     // read the sensors
     // sensor_status.readSensors();
