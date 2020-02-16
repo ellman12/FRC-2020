417734438bb3dca29182bbdd2af6c22ce2a774a8
ellman12 <ellduc4@gmail.com>
2020-02-16 13:59:30 -0600

    idk

--- vs-workspace/Test Code/2020 Mecanum Drive Test/src/main/java/frc/robot/Robot.java
+++ vs-workspace/Test Code/2020 Mecanum Drive Test/src/main/java/frc/robot/Robot.java
@@ -32,21 +32,25 @@ public class Robot extends TimedRobot {
   static String gameData;
 
   // Creating the drive motors.
-  // CANSparkMax frontLeftMotor, frontRightMotor, backLeftMotor, backRightMotor;
+  CANSparkMax frontLeftMotor, frontRightMotor, backLeftMotor, backRightMotor;
 
-  // WPI_TalonFX fLShooter, bLshooter, frshooter, brshooter;
+  WPI_TalonFX fLShooter, bLshooter, frshooter, brshooter;
 
   CANSparkMax rworm, lworm;
 
   WPI_TalonFX intake;
 
-  SpeedControllerGroup wormDrive = new SpeedControllerGroup(rworm, lworm);
-
   // Creating the Mecanum Drive.
-  // MecanumDrive mecanumDrive;
+  MecanumDrive mecanumDrive;
 
   // Creating the PS4 controller.
   Joystick PS4;
+
+  SpeedControllerGroup wormDrive;
+
+  double PS4_Left_X;
+  double PS4_Left_Y;
+  double PS4_Left_Z;
 
   @Override
   public void robotInit() {
@@ -57,15 +61,15 @@ public class Robot extends TimedRobot {
     // backLeftMotor = new WPI_TalonFX(2);
     // backRightMotor = new WPI_TalonFX(4);
 
-    // frontLeftMotor = new CANSparkMax(1, MotorType.kBrushless);
-    // frontRightMotor = new CANSparkMax(2, MotorType.kBrushless);
-    // backLeftMotor = new CANSparkMax(3, MotorType.kBrushless);
-    // backRightMotor = new CANSparkMax(4, MotorType.kBrushless);
+    frontLeftMotor = new CANSparkMax(1, MotorType.kBrushless);
+    frontRightMotor = new CANSparkMax(2, MotorType.kBrushless);
+    backLeftMotor = new CANSparkMax(3, MotorType.kBrushless);
+    backRightMotor = new CANSparkMax(4, MotorType.kBrushless);
 
-    // fLShooter = new WPI_TalonFX(5);
-    // bLshooter = new WPI_TalonFX(7);
-    // frshooter = new WPI_TalonFX(6);
-    // brshooter = new WPI_TalonFX(8);
+    fLShooter = new WPI_TalonFX(5);
+    bLshooter = new WPI_TalonFX(7);
+    frshooter = new WPI_TalonFX(6);
+    brshooter = new WPI_TalonFX(8);
 
     rworm = new CANSparkMax(9, MotorType.kBrushless);
     lworm = new CANSparkMax(10, MotorType.kBrushless);
@@ -73,18 +77,23 @@ public class Robot extends TimedRobot {
     intake = new WPI_TalonFX(11);
 
     // Adding the drive motors to the Mecanum Drive.
-    // mecanumDrive = new MecanumDrive(frontLeftMotor, backLeftMotor,
-    // frontRightMotor, backRightMotor);
+    mecanumDrive = new MecanumDrive(frontLeftMotor, backLeftMotor, frontRightMotor, backRightMotor);
 
     // Assigning the PS4 controller the ID of 0.
     PS4 = new Joystick(0);
 
-    // fLShooter.setNeutralMode(NeutralMode.Brake);
-    // bLshooter.setNeutralMode(NeutralMode.Brake);
-    // frshooter.setNeutralMode(NeutralMode.Brake);
-    // brshooter.setNeutralMode(NeutralMode.Brake);
+    fLShooter.setNeutralMode(NeutralMode.Brake);
+    bLshooter.setNeutralMode(NeutralMode.Brake);
+    frshooter.setNeutralMode(NeutralMode.Brake);
+    brshooter.setNeutralMode(NeutralMode.Brake);
 
-    lworm.setInverted(true);
+    rworm.setIdleMode(IdleMode.kBrake);
+    lworm.setIdleMode(IdleMode.kBrake);
+
+    rworm.setInverted(true);
+
+    wormDrive = new SpeedControllerGroup(rworm, lworm);
+
   }
 
   @Override
@@ -103,16 +112,26 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
 
+    if (PS4.getRawButton(9) == true) {
+      fLShooter.set(0.3);
+      frshooter.set(-0.3);
+    } else {
+      fLShooter.set(0);
+      frshooter.set(0);
+    }
+
     if (PS4.getRawButton(1) == true) {
       // fLShooter.set(0.3);
       // frshooter.set(-0.3);
       // brshooter.set(-0.3);
       // bLshooter.set(0.3);
-      wormDrive.set(0.3);
+      // wormDrive.set(0.3);
+      wormDrive.set(0.10);
 
     } else if (PS4.getRawButton(2) == true) {
 
-      wormDrive.set(-0.3);
+      // wormDrive.set(-0.3);
+      wormDrive.set(-0.10);
 
     } else {
       // fLShooter.set(0.0);
@@ -123,15 +142,31 @@ public class Robot extends TimedRobot {
     }
 
     if (PS4.getRawButton(3) == true) {
-      intake.set(0.3);
+      intake.set(-0.3);
     } else {
       intake.set(0);
     }
 
-    // backRightMotor.set(0.5);
+    if (PS4.getRawButton(5) == true) {
+      bLshooter.set(0.1);
+      brshooter.set(-0.1);
+    } else {
+      bLshooter.set(0.0);
+      brshooter.set(0.0);
+    }
 
     // Drive the robot's Mecanum Drive with the PS4 controller.
-    // mecanumDrive.driveCartesian(PS4.getY(), PS4.getX(), PS4.getZ());
+
+    PS4_Left_X = PS4.getX();
+    PS4_Left_Y = PS4.getY();
+    PS4_Left_Z = PS4.getZ();
+
+    if (((PS4.getX() > 0.2) && (PS4.getY() > 0.2) && (PS4.getZ() > 0.2))
+        || ((PS4.getX() < -0.2) && (PS4.getY() < -0.2) && (PS4.getZ() < -0.2))) {
+      mecanumDrive.driveCartesian(PS4.getZ(), PS4.getX(), PS4.getY(), 0);
+    }
+
+    // mecanumDrive.driveCartesian(PS4.getX(), PS4.getY(), PS4.getZ(), 0);
 
     // IDK about the Z axis.
     // Our 2nd year robot (Kova) could strafe using the 2 analog triggers. We should
