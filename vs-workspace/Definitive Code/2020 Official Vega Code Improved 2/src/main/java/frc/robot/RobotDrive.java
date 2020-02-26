e9e142110c47ddde48c3a1b9b2093aafa781dc48
ellman12 <ellduc4@gmail.com>
2020-02-25 20:28:48 -0600

    idk

--- vs-workspace/Definitive Code/2020 Official Vega Code Improved 2/src/main/java/frc/robot/RobotDrive.java
+++ vs-workspace/Definitive Code/2020 Official Vega Code Improved 2/src/main/java/frc/robot/RobotDrive.java
@@ -19,21 +19,18 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 class RobotDrive {
 
-    // Constructor.
-    RobotDrive() {
+        // Constructor.
+        RobotDrive() {
 
-        // Creating a new instance of the Robot.java's implicit constructor in here.
-        // Robot robot = new Robot();
+                // Creating the drive motors.
+                CANSparkMax frontLeftDriveMotor = new CANSparkMax(VarsAndConsts.FRONT_LEFT_DRIVE_MOTOR_ID,
+                                MotorType.kBrushless);
+                CANSparkMax frontRightDriveMotor = new CANSparkMax(VarsAndConsts.FRONT_RIGHT_DRIVE_MOTOR_ID,
+                                MotorType.kBrushless);
+                CANSparkMax backLeftDriveMotor = new CANSparkMax(VarsAndConsts.BACK_LEFT_DRIVE_MOTOR_ID,
+                                MotorType.kBrushless);
+                CANSparkMax backRightDriveMotor = new CANSparkMax(VarsAndConsts.BACK_RIGHT_DRIVE_MOTOR_ID,
+                                MotorType.kBrushless);
 
-        // Creating the drive motors.
-        CANSparkMax frontLeftDriveMotor = new CANSparkMax(VarsAndConsts.FRONT_LEFT_DRIVE_MOTOR_ID,
-                MotorType.kBrushless);
-        CANSparkMax frontRightDriveMotor = new CANSparkMax(VarsAndConsts.FRONT_RIGHT_DRIVE_MOTOR_ID,
-                MotorType.kBrushless);
-        CANSparkMax backLeftDriveMotor = new CANSparkMax(Robot.varsAndConsts.BACK_LEFT_DRIVE_MOTOR_ID,
-                MotorType.kBrushless);
-        CANSparkMax backRightDriveMotor = new CANSparkMax(Robot.varsAndConsts.BACK_RIGHT_DRIVE_MOTOR_ID,
-                MotorType.kBrushless);
-
-    }
+        }
 }
