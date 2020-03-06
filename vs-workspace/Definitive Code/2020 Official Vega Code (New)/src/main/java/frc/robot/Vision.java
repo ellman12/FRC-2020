927fd187fcee81e3d5d6da649760bae80ad221d1
ellman12 <ellduc4@gmail.com>
2020-03-05 20:23:30 -0600

    Update Vision.java

--- vs-workspace/Definitive Code/2020 Official Vega Code (New)/src/main/java/frc/robot/Vision.java
+++ vs-workspace/Definitive Code/2020 Official Vega Code (New)/src/main/java/frc/robot/Vision.java
@@ -1,1 +1,35 @@
-// Empty for now...
+/////////////////////////////////////////////////////////////////////
+// File: Vision.java
+/////////////////////////////////////////////////////////////////////
+//
+// Purpose: Used for vision stuff for the Limelight.
+//
+// Authors: Elliott DuCharme.
+//
+// Environment: Microsoft VSCode Java.
+//
+// Remarks: Created on 3/05/2020.
+//
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+package frc.robot;
+
+// TODO Limelight Ideas:
+
+// Limelight NetworkTable Ideas:
+// http://docs.limelightvision.io/en/latest/networktables_api.html
+
+// camMode: 0 when auto and lining up; 1 when driving around. Some kind of magic code for this.
+
+// stream: for switching between primary and secondary camera views.
+
+// snapshot: can take pics of vision targets and calibrate in pits or something.
+
+// Good (example) code: https://github.com/GraniteCityGearheads3244/2019_Jupiter/blob/master/src/main/java/oi/limelightvision/limelight/frc/LimeLight.java
+// https://github.com/GraniteCityGearheads3244/LimeLight_Library_Example/tree/master/src/main/java/oi/limelightvision/limelight/frc
+// https://youtu.be/-E7XNvl3j8A?t=1010
+
+// Extends Auto which also extends Robot.
+class Vision extends Autonomous {
+
+}
